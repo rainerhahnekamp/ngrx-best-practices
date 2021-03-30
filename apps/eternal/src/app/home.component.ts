9d1d3f8ee27d8995ec51d66de091cf914a9836f3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-03-30 12:11:38 +0200

    fix linting

---
+++ apps/eternal/src/app/home.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<p>Eternal is an imaginary travel agency and is used as training application for Angular developers.</p>
+    <p>You can click around, do whatever you want but don't expect to be able to book a real holiday 😉.</p> `
+})
+export class HomeComponent {}
